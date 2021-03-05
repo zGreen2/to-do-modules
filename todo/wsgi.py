f521da0105fc832a4e0210606a42fffb51ed7bb6
Mark Mamore <63468384+zGreen2@users.noreply.github.com>
2021-03-05 20:47:53 +0800

    Update wsgi.py

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -1,11 +1,9 @@
-"""
-WSGI config for todo project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
+application = Cling(get_wsgi_application())
+
 
 import os
 
